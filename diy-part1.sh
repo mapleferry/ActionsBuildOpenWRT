7c306e29843b0cff32ce14cf9c4fb123bbe9fe78
mapleferry <mapleferry@sina.com>
2025-12-07 20:36:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,11 +5,9 @@
 
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
-sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' ./feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' ./feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' ./feeds.conf.default
 
 # homeproxy
 #git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
